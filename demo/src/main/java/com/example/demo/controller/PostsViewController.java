50b0ef8d7ccb44c3e16326ca1b8b49f33aa18df2
monkeybisness <User@DESKTOP-MIVQ3QL>
2024-02-19 01:19:52 +0500

    Sixth commit

--- demo/src/main/java/com/example/demo/controller/PostsViewController.java
+++ demo/src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
+    @Autowired
+    PostService postsService;
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
 
